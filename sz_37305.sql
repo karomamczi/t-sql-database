f614074e972ab7a0fc475da6e6b4a9a3324df02a
Karolina Mamczarz <karolina.mamczarz@op.pl>
2018-06-06 23:49:46 +0200

    refactor: Add authorization to schema creation feat: Create table and primary key

--- sz_37305.sql
+++ sz_37305.sql
@@ -3,4 +3,19 @@ CREATE DATABASE sz_37305
 USE sz_37305
 GO
 
-CREATE SCHEMA Euref
+CREATE SCHEMA Euref AUTHORIZATION [dbo]
+GO
+
+CREATE TABLE Euref.Station (
+  StationId int IDENTITY NOT NULL,
+  Name nchar(9),
+  City nvarchar(50),
+  Country nvarchar(50),
+  Latitude decimal(6,4),
+  Longitude decimal(7,4),
+  Height decimal(4,1),
+  ModifiedDate datetime
+)
+
+ALTER TABLE Euref.Station ADD CONSTRAINT PK_Station PRIMARY KEY (StationId)
+GO
