2e7d8a55467ee915180059a958ea7e7d256e7c43
Karolina Mamczarz <karolina.mamczarz@op.pl>
2018-06-09 16:52:48 +0200

    feat: Create other tables

--- sz_37305.sql
+++ sz_37305.sql
@@ -17,5 +17,49 @@ CREATE TABLE Euref.Station (
   ModifiedDate datetime
 )
 
+CREATE TABLE Euref.TrackSystem (
+  StationId int IDENTITY NOT NULL,
+  TrackGps bit,
+  TrackGlo bit,
+  TrackGal bit,
+  TrackBds bit,
+  ModifiedDate datetime
+)
+
+CREATE TABLE Euref.StationConfiguration (
+  StationId int IDENTITY NOT NULL,
+  IntegrationDate datetime,
+  OperatingInstitutionId int,
+  ReceiverId int,
+  AntennaId int,
+  IndividualCalibration	bit,
+  ModifiedDate datetime
+)
+
+CREATE TABLE Euref.Institution (
+  OperatingInstitutionId int IDENTITY NOT NULL,
+  Name nvarchar(200),
+  Acronym nchar(6),
+  AddressLine nvarchar(200),
+  PostalCode nvarchar(20),
+  City nvarchar(50),
+  Country nvarchar(50),	
+  ModifiedDate datetime
+)
+
+CREATE TABLE Euref.Receiver (
+  ReceiverId int IDENTITY NOT NULL,
+  Name nvarchar(50),	
+  ModifiedDate datetime
+)
+
+CREATE TABLE Euref.Antenna (
+  AntennaId int IDENTITY NOT NULL,
+  Name nvarchar(50),
+  ModifiedDate datetime
+)
+
+
 ALTER TABLE Euref.Station ADD CONSTRAINT PK_Station PRIMARY KEY (StationId)
-GO
+
+
